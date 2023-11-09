8b901bb6f041ef42e7ce0a16b1f83b2f0d533364
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2023-11-09 10:31:07 +0700

    feat: optimizing performance (wip)

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,5 +1,38 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
 import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
+
+const DIST_PATH = './dist';
+
+async function addDirectivesToChunkFiles(distPath = DIST_PATH): Promise<void> {
+  try {
+    const files = await fs.readdir(distPath);
+
+    for (const file of files) {
+      if (
+        file.startsWith('chunk-') &&
+        (file.endsWith('.mjs') || file.endsWith('.js'))
+      ) {
+        const filePath = path.join(distPath, file);
+
+        // eslint-disable-next-line no-await-in-loop -- We need to wait for each file to be read
+        const data = await fs.readFile(filePath, 'utf8');
+
+        const updatedContent = `'use client';\n${data}`;
+
+        // eslint-disable-next-line no-await-in-loop -- We need to wait for each file to be written
+        await fs.writeFile(filePath, updatedContent, 'utf8');
+
+        // eslint-disable-next-line no-console -- We need to log the result
+        console.log(`Directive has been added to ${file}`);
+      }
+    }
+  } catch (err) {
+    // eslint-disable-next-line no-console -- We need to log the error
+    console.error('Error:', err);
+  }
+}
 
 export default defineConfig((options: Options) => ({
   clean: !options.watch,
@@ -7,6 +40,10 @@ export default defineConfig((options: Options) => ({
   entry: ['src/**/*.tsx'],
   external: ['react'],
   format: ['esm', 'cjs'],
+  async onSuccess() {
+    await addDirectivesToChunkFiles();
+  },
+  outDir: DIST_PATH,
   sourcemap: true,
   splitting: true,
   target: 'esnext',
